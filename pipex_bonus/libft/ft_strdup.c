645f461669ec56dc3ab7f1bfcbb20dcd23934974
mohammed <71414472+mohammedhrima@users.noreply.github.com>
2022-12-22 02:49:27 +0100

    Add files via upload

---
+++ pipex_bonus/libft/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhrima <mhrima@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 23:45:08 by mhrima            #+#    #+#             */
+/*   Updated: 2022/12/09 01:35:28 by mhrima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*ptr;
+	int		i;
+
+	ptr = (char *)ft_calloc((ft_strlen(s) + 1), sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
