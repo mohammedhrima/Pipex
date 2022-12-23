aee2e24db3151b33e86a65edb040a326d41b8815
mohammed <71414472+mohammedhrima@users.noreply.github.com>
2022-12-23 15:15:46 +0100

    Add files via upload

--- pipex_bonus_1/pipex.h
+++ pipex_bonus_1/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mhrima <mhrima@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 08:09:37 by mhrima            #+#    #+#             */
-/*   Updated: 2022/12/23 14:42:36 by mhrima           ###   ########.fr       */
+/*   Updated: 2022/12/13 16:57:56 by mhrima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,5 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include "libft/libft.h"
-#include <errno.h>
-#include <string.h>
-
-char *get_path_from_env_path(char *cmd1, char **paths);
-char *cmd_path(char *cmd0, char **paths);
-void Free(char **arr);
-char *grep(char *arr[], char *str);
-void here_doc(int argc, char **argv, char **envp, int *status);
-void multi_Pipes(int argc, char **argv, char **envp, int *status);
-void *allocate1(int len, size_t size);
-void **allocate2(int len, size_t size, int sublen, size_t subsize);
-void open_pipes(int **pipes, int *i, int len);
 
 #endif
-
-/*
-protect all mallocs
-*/
--- pipex_bonus_2/pipex.c
+++ pipex_bonus_2/pipex.c
@@ -1,22 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex2.c                                           :+:      :+:    :+:   */
+/*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhrima <mhrima@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 04:46:12 by mhrima            #+#    #+#             */
-/*   Updated: 2022/12/23 07:20:37 by mhrima           ###   ########.fr       */
+/*   Updated: 2022/12/23 14:56:47 by mhrima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+// #include "Leak_Hunter/leak_hunter.h"
 
-char *get_path_from_env_path(char *cmd1, char **paths)
+char *get_path_from_env_path(char *cmd1, char **envp)
 {
 	char *cmd2;
-	int	i;
-
+	char **paths;
+	int i;
+	
+	paths = ft_split(grep(envp, "PATH"), ':');
 	i = 0;
 	while (paths && cmd1 && paths[i])
 	{
@@ -24,6 +27,7 @@ char *get_path_from_env_path(char *cmd1, char **paths)
 		if (access(cmd2, F_OK) == 0 && access(cmd2, X_OK) == 0)
 		{
 			free(cmd1);
+			Free(paths);
 			return cmd2;
 		}
 		free(cmd2);
@@ -31,10 +35,11 @@ char *get_path_from_env_path(char *cmd1, char **paths)
 		i++;
 	}
 	free(cmd1);
+	Free(paths);
 	return (cmd2);
 }
 
-char *cmd_path(char *cmd0, char **paths)
+char *cmd_path(char *cmd0, char **envp)
 {
 	int i;
 	char *cmd1;
@@ -44,13 +49,13 @@ char *cmd_path(char *cmd0, char **paths)
 	cmd2 = NULL;
 	if (access(cmd0, F_OK) == 0 && access(cmd0, X_OK) == 0)
 		return cmd0;
-	if(cmd0 && cmd0[0] != '/')
+	if (cmd0 && cmd0[0] != '/')
 		cmd1 = ft_strjoin("/", cmd0);
 	else
 		cmd1 = ft_strdup(cmd0);
 	if (access(cmd1, F_OK) == 0 && access(cmd1, X_OK) == 0)
 		return cmd1;
-	return (get_path_from_env_path(cmd1, paths));
+	return (get_path_from_env_path(cmd1, envp));
 }
 
 void Free(char **arr)
@@ -80,235 +85,72 @@ char *grep(char *arr[], char *str)
 	}
 	return NULL;
 }
+
+void *allocate1(int len, size_t size)
+{
+	void *res;
+
+	res = malloc(len * size);
+	if (!res)
+		exit(1);
+	return res;
+}
+void **allocate2(int len, size_t size, int sublen, size_t subsize)
+{
+	void **res;
+	int i;
+
+	i = 0;
+	res = malloc(len * size);
+	if (!res)
+		exit(1);
+	while (i < len)
+	{
+		res[i] = malloc(sublen * subsize);
+		if (!res[i])
+		{
+			while (i > 0)
+			{
+				free(res[i]);
+				i--;
+			}
+			free(res);
+			exit(1);
+		}
+		i++;
+	}
+	return res;
+}
+
+void open_pipes(int **pipes, int *i, int len)
+{
+	while (*i < len)
+	{
+		if (pipe(pipes[*i]) == -1)
+		{
+			while (i > 0)
+			{
+				close(pipes[*i][0]);
+				close(pipes[*i][1]);
+				i--;
+			}
+			perror("Error: ");
+			exit(-1);
+		}
+		i++;
+	}
+}
+
 int main(int argc, char **argv, char **envp)
 {
-	int num;
-	int *pids;
-	int **pipes;
-	int pipes2[2];
-	int i;
-	char **arr;
-	char *cmd;
-	char *str;
-	char **paths;
-	char *Error;
 	int status;
-	char *Exit;
 
 	if (argc < 5)
 		exit(1);
 	if (ft_strcmp(argv[1], "here_doc") == 0)
-	{
-		Exit = argv[2];
-		if (pipe(pipes2) == -1)
-		{
-			perror("Error: ");
-			exit(-1);
-		}
-		str = NULL;
-		while (ft_strcmp(str, Exit))
-		{
-			str = get_next_line(STDIN_FILENO);
-			ft_putendl_fd(str, pipes2[1]);
-			free(str);
-		}
-		str = NULL;
-		close(pipes2[0]);
-		close(pipes2[1]);
-		// execute commands
-		pids = (int *)malloc(3 * sizeof(int));
-		pipes = (int **)malloc(3 * sizeof(int *));
-		i = 0;
-		while (i < 3)
-		{
-			pipes[i] = (int *)malloc(2 * sizeof(int));
-			i++;
-		}
-		cmd = NULL;
-		arr = NULL;
-		pipes[2][1] = open(argv[argc - 1], O_WRONLY | O_APPEND | O_CREAT, 0644);
-		
-		if (pipes[2][1] < 0)
-		{
-			Error = ft_strjoin("permission denied: ", argv[argc - 1]);
-			ft_putendl_fd(Error, 2);
-			free(Error);
-			exit(1);
-		}
-		paths = ft_split(grep(envp, "PATH"), ':');
-		i = 0;
-		while (i < 2)
-		{
-			if (pipe(pipes[i]) == -1)
-			{
-				perror("Error: ");
-				exit(-1);
-			}
-			i++;
-		}
-		i = 0;
-		while (i < 2)
-		{
-			pids[i] = fork();
-			if (pids[i] < 0)
-			{
-				perror("Error: ");
-				exit(-1);
-			}
-			if (pids[i] == 0)
-			{
-				arr = ft_split2(argv[3 + i], ' ', 3);
-				cmd = cmd_path(arr[0], paths);
-				if (!cmd)
-				{
-					Error = ft_strjoin("command not found: ", arr[0]);
-					ft_putendl_fd(Error, 2);
-					free(Error);
-				}
-				Free(paths);
-				int j = 0;
-				while (j < 2)
-				{
-					if (i != j)
-						close(pipes[j][0]);
-					if (i + 1 != j)
-						close(pipes[j][1]);
-					j++;
-				}
-				dup2(pipes[i][0], STDIN_FILENO);
-				dup2(pipes[i + 1][1], STDOUT_FILENO);
-				if (execve(cmd, arr, envp) == -1)
-				{
-					if (i == 2)
-						exit(127);
-					else
-						exit(0);
-				}
-			}
-			else if (pids[0] > 0)
-			{
-				close(pipes[i][0]);
-				close(pipes[i][1]);
-				cmd = NULL;
-				arr = NULL;
-			}
-			i++;
-		}
-		i = 0;
-		while (i < 2)
-		{
-			waitpid(pids[i], &status, 0);
-			i++;
-		}
-	}
+		here_doc(argc, argv, envp, &status);
 	else
-	{
-		num = argc - 3;
-		pids = (int *)malloc(num * sizeof(int));
-		pipes = (int **)malloc((num + 1) * sizeof(int *));
-		i = 0;
-		while (i < num + 1)
-		{
-			pipes[i] = (int *)malloc(2 * sizeof(int));
-			i++;
-		}
-		cmd = NULL;
-		arr = NULL;
-		
-		pipes[0][0] = open(argv[1], O_RDONLY);
-		pipes[num][1] = open(argv[argc - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
-		if (pipes[num][1] < 0)
-		{
-			Error = ft_strjoin("permission denied: ", argv[argc - 1]);
-			ft_putendl_fd(Error, 2);
-			free(Error);
-			exit(1);
-		}
-		if (pipes[0][0] < 0)
-		{
-			Error = ft_strjoin("No such file or directory: ", argv[1]);
-			ft_putendl_fd(Error, 2);
-			free(Error);
-		}
-		i = 1;
-		paths = ft_split(grep(envp, "PATH"), ':');
-		while (i < num)
-		{
-			if (pipe(pipes[i]) == -1)
-			{
-				perror("Error: ");
-				exit(-1);
-			}
-			i++;
-		}
-		i = 0;
-		while (i < num)
-		{
-
-			pids[i] = fork();
-			if (pids[i] == -1)
-			{
-				perror("Error: ");
-				exit(-1);
-			}
-			if (pids[i] == 0)
-			{
-				if (pipes[i][0] >= 0)
-				{
-					arr = ft_split2(argv[i + 2], ' ', 3);
-					cmd = cmd_path(arr[0], paths);
-					if (!cmd)
-					{
-						Error = ft_strjoin("command not found: ", arr[0]);
-						ft_putendl_fd(Error, 2);
-						free(Error);
-					}
-				}
-				Free(paths);
-				int j = 0;
-				while (j < num + 1)
-				{
-					if (i != j)
-						close(pipes[j][0]);
-					if (i + 1 != j)
-						close(pipes[j][1]);
-					j++;
-				}
-				dup2(pipes[i][0], STDIN_FILENO);
-				dup2(pipes[i + 1][1], STDOUT_FILENO);
-				if (execve(cmd, arr, envp) == -1)
-				{
-					if (i == 2)
-						exit(127);
-					else
-						exit(0);
-				}
-					
-			}
-			else if (pids[i] > 0)
-			{
-				close(pipes[i][0]);
-				close(pipes[i + 1][1]);
-				cmd = NULL;
-				arr = NULL;
-			}
-			i++;
-		}
-		int j = 0;
-		while (j < num + 1)
-		{
-			if (i != j)
-				close(pipes[j][0]);
-			if (i + 1 != j)
-				close(pipes[j][1]);
-			j++;
-		}
-		i = 0;
-		while (i < num)
-		{
-			waitpid(pids[i], &status, 0);
-			i++;
-		}
-	}
+		multi_Pipes(argc, argv, envp, &status);
+	//system("leaks pipex");
 	return (WEXITSTATUS(status));
 }
